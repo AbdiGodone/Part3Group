55af920e59db97eb4be0a015a6b785bafc719b40
AbdiGodone <abdigodone@gmail.com>
2024-04-18 01:23:43 +0100

    2 methods calcPosition add

--- src/main/java/Part3/BoundedPriorityQueuSet.java
+++ src/main/java/Part3/BoundedPriorityQueuSet.java
@@ -7,30 +7,57 @@ public class BoundedPriorityQueuSet {
     private int Maxsize;
 
 
-    public BoundedPriorityQueuSet(){
+    public BoundedPriorityQueuSet() {
         BoundedPriorityQueueSet = new LinkedList<>();
         this.Maxsize = 10;
     }
 
     public BoundedPriorityQueuSet(int Maxsize) {
-        if (Maxsize <= 0){
+        if (Maxsize <= 0) {
             throw new IllegalArgumentException("max size needs to be greater than 0");
 
         }
-       this.BoundedPriorityQueueSet = new LinkedList<>();
+        this.BoundedPriorityQueueSet = new LinkedList<>();
         this.Maxsize = Maxsize;
     }
 
-    public int size(){
+    public int size() {
         return BoundedPriorityQueueSet.size();
     }
 
-    public boolean isFull(){
+    public boolean isFull() {
         return size() == Maxsize;
     }
 
+    private int calcPosition(Task task) {
+        int position = 0;
+        for (Task task1 : BoundedPriorityQueueSet) {
+            if (task1.equals(task)) {
+                throw new DuplicateElementException("this task already present");
+            }
+            if (task1.getDeadline().compareTo(task.getDeadline()) > 0) {
+                position++;
+
+            }
+            return position;
+        }
+    }
+
+        public void add (Task task){
+            if (isFull()) {
+                throw new IllegalStateException("the que is full");
+            }
+            for (Task task1 : BoundedPriorityQueueSet){
+                if (task1.equals(task)){
+                    throw new DuplicateElementException("this task already exists");
+
+                }
+            }
+
+            BoundedPriorityQueueSet.add(calcPosition(task), task);
+
+        }
 
 
+    }
 
-
-}
